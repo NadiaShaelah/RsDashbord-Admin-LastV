cea20bfd27de9add3edd20d26dc5e316e2b5afc3
rschain5 <shaelah.zebato@rschain.net>
2024-09-09 11:48:09 +0000

    maj

--- src/components/Main/Orders/Orders.jsx
+++ src/components/Main/Orders/Orders.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 import { OrdersTab, OrdersTabs } from './OrdersTabs'
 
 import { database } from '../../../firebase-config'
-import { getDatabase, onValue, ref, update, query, limitToLast, orderByChild, orderByValue } from "firebase/database";
+import { getDatabase, onValue, ref, update, query, orderByChild, orderByValue } from "firebase/database";
 
 
 import { where, orderBy, getDocs } from "firebase/firestore";
